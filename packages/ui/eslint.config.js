19b62a6cb207a6bb83076467429504ca19d69110
tiesen243 <ttien56906@gmail.com>
2024-09-17 03:32:44 +0700

    feat: update dependencies and configs for UI package
    - Added new devDependencies to package.json
    - Updated eslint.config.js to include reactConfig
    - Modified tsconfig.json to include new compiler options
    - Updated components.json tailwind config path
    - Added new exports and utility function in src/index.ts
    - Updated scripts in package.json for better development workflow

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,9 +1,11 @@
 import baseConfig from '@yuki/eslint-config/base'
+import reactConfig from '@yuki/eslint-config/react'
 
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [],
+    ignores: ['dist/**'],
   },
   ...baseConfig,
+  ...reactConfig,
 ]
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,12 @@
-export const name = 'ui'
+import type { ClassValue } from 'clsx'
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+export { cn }
+
+export * from 'geist/font/mono'
+export * from 'geist/font/sans'
+export * as icons from 'lucide-react'
+export * from 'next-themes'
