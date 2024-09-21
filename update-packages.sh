d74485f02cbfd93f91232245061e80bf71e414f2
tiesen243 <ttien56906@gmail.com>
2024-09-21 17:37:33 +0700

    feat(update): add outdated check before updating packages
    Added a check to display outdated packages before updating them in the
    update-packages.sh script. This helps in identifying which packages
    are outdated before proceeding with the update.

--- update-packages.sh
+++ update-packages.sh
@@ -12,9 +12,13 @@ for BASE_DIR in "${BASE_DIRS[@]}"; do
 
     # Check if the package is in the packages/ui directory
     if [[ "$PACKAGE_DIR" == *"packages/ui"* ]]; then
+      echo "Outdated packages in $PACKAGE_DIR"
+      (cd "$PACKAGE_DIR" && bun outdated)
       echo "Updating $PACKAGE_DIR with bun update --latest"
       (cd "$PACKAGE_DIR" && bun update --latest)
     else
+      echo "Outdated packages in $PACKAGE_DIR"
+      (cd "$PACKAGE_DIR" && bun outdated)
       echo "Updating $PACKAGE_DIR with bun update"
       (cd "$PACKAGE_DIR" && bun update)
     fi
