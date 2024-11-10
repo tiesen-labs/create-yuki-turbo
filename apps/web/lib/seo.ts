eded9bc8682c99e258976bd931ee6d5e028a3a72
tiesen243 <ttien56906@gmail.com>
2024-11-11 01:01:36 +0700

    feat(seo): add siteName to openGraph metadata
    - Introduced `siteName` constant for better maintainability.
    - Moved `url` initialization to improve readability.
    - Updated `applicationName` and `openGraph` metadata to use `siteName`.

--- apps/web/lib/seo.ts
+++ apps/web/lib/seo.ts
@@ -5,25 +5,26 @@ import { getBaseUrl } from '@/lib/utils'
 interface Prams {
   title?: string
   description?: string
-  url?: string
   images?: string[]
+  url?: string
 }
 
 export const seo = (params: Prams): Metadata => {
+  const siteName = 'Create Yuki Turbo'
   const title = params.title ? `${params.title} | Create Yuki Turbo` : 'Create Yuki Turbo'
   const description =
     params.description ?? 'Clean and typesafe starter monorepo using Turborepo along with Next.js'
-  const url = params.url ? `${getBaseUrl()}${params.url}` : getBaseUrl()
   const images = [...(params.images ?? []), '/api/og']
+  const url = params.url ? `${getBaseUrl()}${params.url}` : getBaseUrl()
 
   return {
     metadataBase: new URL(getBaseUrl()),
     title,
     description,
-    applicationName: 'Create Yuki Turbo',
-    alternates: { canonical: url },
-    openGraph: { url, images, type: 'website' },
+    applicationName: siteName,
     twitter: { card: 'summary_large_image' },
+    openGraph: { url, images, siteName, type: 'website' },
     icons: { icon: 'https://tiesen.id.vn/favicon.ico' },
+    alternates: { canonical: url },
   }
 }
