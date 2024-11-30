d0cac04a37d5fd3cd5ed6d8118622d6e29c06f14
tiesen243 <ttien56906@gmail.com>
2024-12-01 02:41:19 +0700

    fix(ci): format error

--- tooling/tailwind/web.ts
+++ tooling/tailwind/web.ts
@@ -11,9 +11,9 @@ const config = {
     extend: {
       container: { center: true, padding: '2rem', screens: { '2xl': '1400px' } },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
