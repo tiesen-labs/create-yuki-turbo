a42ca2f1f5a4b4a5de62b504d51232fa486782d9
tiesen243 <ttien56906@gmail.com>
2024-12-01 02:38:30 +0700

    chore: disable edge runtime

--- apps/www/app/api/auth/[...auth]/route.ts
+++ apps/www/app/api/auth/[...auth]/route.ts
@@ -2,6 +2,6 @@ import { oauth } from '@yuki/auth/oauth'
 
 import { getBaseUrl } from '@/lib/utils'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 
 export const GET = oauth(getBaseUrl())
