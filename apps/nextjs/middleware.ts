58e5f38a41d9214c3834fed2cd7b287eb5747592
tiesen243 <ttien56906@gmail.com>
2024-12-03 22:22:47 +0700

    chore: add middleware example

---
+++ apps/nextjs/middleware.ts
@@ -0,0 +1,15 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+import { auth } from '@yuki/auth'
+
+export default async (req: NextRequest) => {
+  //   const session = await auth() //  { session: { user: { ... } } }
+  return NextResponse.next()
+}
+
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
+
