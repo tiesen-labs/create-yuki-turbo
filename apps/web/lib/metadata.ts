9dcd278b2f675a253254a451ed315d8d8d17d2fc
tiesen243 <ttien56906@gmail.com>
2025-01-26 04:15:37 +0700

    feat(web): add og generate route

--- apps/web/lib/metadata.ts
+++ apps/web/lib/metadata.ts
@@ -6,22 +6,22 @@ export const createMetadata = (
   override: Omit<Metadata, 'title'> & { title?: string },
 ): Metadata => {
   const siteName = 'Create Yuki Turbo'
+  const description =
+    'Clean and typesafe starter monorepo using Turborepo along with Next.js and tRPC '
 
   const url = override.openGraph?.url
     ? `${getBaseUrl()}${override.openGraph.url}`
     : getBaseUrl()
   const images = [
     ...((override.openGraph?.images as [] | null) ?? []),
-    'https://tiesen.id.vn/api/og', // Or create your own API route to generate OG images in `/app/api/og`
+    `/api/og?title=${encodeURIComponent(override.title ?? siteName)}&description=${encodeURIComponent(override.description ?? description)}`,
   ]
 
   return {
     ...override,
     metadataBase: new URL(getBaseUrl()),
     title: override.title ? `${siteName} | ${override.title}` : siteName,
-    description:
-      override.description ??
-      'Clean and typesafe starter monorepo using Turborepo along with Next.js and tRPC ',
+    description: override.description ?? description,
     applicationName: siteName,
     alternates: { canonical: url },
     twitter: { card: 'summary_large_image' },
