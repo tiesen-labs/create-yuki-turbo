de31ec64ae0256588f7c2ea8cc9090a873f196c8
tiesen243 <ttien56906@gmail.com>
2025-03-22 20:49:43 +0700

    chore(lucide-react): use import with suffix config

--- apps/web/lib/trpc/react.tsx
+++ apps/web/lib/trpc/react.tsx
@@ -3,11 +3,7 @@
 import type { QueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
-import {
-  createTRPCClient,
-  loggerLink,
-  unstable_httpBatchStreamLink,
-} from '@trpc/client'
+import { createTRPCClient, httpBatchStreamLink, loggerLink } from '@trpc/client'
 import { createTRPCContext } from '@trpc/tanstack-react-query'
 import SuperJSON from 'superjson'
 
@@ -39,7 +35,7 @@ export const TRPCReactProvider: React.FC<{ children: React.ReactNode }> = ({
             process.env.NODE_ENV === 'development' ||
             (op.direction === 'down' && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
+        httpBatchStreamLink({
           transformer: SuperJSON,
           url: getBaseUrl() + '/api/trpc',
           headers() {
