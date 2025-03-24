58cd425c900fb31cc45c8c70a350dae13640c704
tiesen243 <ttien56906@gmail.com>
2025-03-24 16:26:40 +0700

    chore(ui): using tw-animate-css intead of tailwindcss-animate

--- apps/dashboard/app/routes/api.trpc.$trpc.ts
+++
@@ -1,6 +0,0 @@
-import { handlers } from '@yuki/api'
-
-import type { Route } from './+types/api.trpc.$trpc'
-
-export const loader = async ({ request }: Route.LoaderArgs) => handlers(request)
-export const action = async ({ request }: Route.ActionArgs) => handlers(request)
--- packages/ui/src/icons.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@yuki/ui/icons' {
-  export * from 'lucide-react/dist/lucide-react.suffixed'
-
-  export const DiscordIcon: React.FC<React.ComponentProps<'svg'>>
-  export const GoogleIcon: React.FC<React.ComponentProps<'svg'>>
-}
