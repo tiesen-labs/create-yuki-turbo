fec27eed22a9d0d4528f2f6b3522e8c17a2b7525
tiesen243 <ttien56906@gmail.com>
2025-03-31 00:23:37 +0700

    chore(react-router): enable prerender

--- apps/react-router/react-router.config.ts
+++ apps/react-router/react-router.config.ts
@@ -5,5 +5,10 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  prerender: true,
+  future: {
+    unstable_optimizeDeps: true,
+    unstable_splitRouteModules: true,
+  },
   presets: [vercelPreset()],
 } satisfies Config
