48e5245fd9b24b6b78f918733f5499691156838f
tiesen243 <ttien56906@gmail.com>
2025-04-03 19:53:02 +0700

    chore(react-router): use `src` intead of `app`

--- apps/react-router/react-router.config.ts
+++ apps/react-router/react-router.config.ts
@@ -3,12 +3,13 @@ import { vercelPreset } from '@vercel/react-router/vite'
 
 export default {
   // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
-  prerender: true,
+  appDirectory: 'src',
   future: {
     unstable_optimizeDeps: true,
     unstable_splitRouteModules: true,
   },
   presets: [vercelPreset()],
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
+  prerender: true,
 } satisfies Config
