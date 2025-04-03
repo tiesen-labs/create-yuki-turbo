1b951c319938222e8f346591dee9b1dd5ac5059d
tiesen243 <ttien56906@gmail.com>
2025-04-03 21:52:37 +0700

    chore: edge

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,6 @@
 import { neonConfig, Pool } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
-import { getPosts } from '@prisma/client/sql'
 
 neonConfig.poolQueryViaFetch = true
 
@@ -23,7 +22,5 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db = globalForPrisma.prisma ?? createPrismaClient()
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
-void db.$queryRawTyped(getPosts())
 
 export type * from '@prisma/client'
