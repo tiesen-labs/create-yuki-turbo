23cb87742babeb74085758c2ba40d3c003ad4fb7
tiesen243 <ttien56906@gmail.com>
2025-04-10 01:30:43 +0700

    fix: build error

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,8 +1,7 @@
 import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
 import ws from 'ws'
-
-import { PrismaClient } from '../generated/client'
 
 neonConfig.webSocketConstructor = ws
 neonConfig.poolQueryViaFetch = true
@@ -27,4 +26,4 @@ const globalForPrisma = globalThis as unknown as {
 export const db = globalForPrisma.prisma ?? createPrismaClient()
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
-export type * from '../generated/client'
+export type * from '@prisma/client'
