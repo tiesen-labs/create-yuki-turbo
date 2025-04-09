3c382a9e393ff0986753ecd48f3aab0bcc5b6736
tiesen243 <ttien56906@gmail.com>
2025-04-10 03:45:25 +0700

    chore: bump next to 15.3.0

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -58,5 +58,7 @@ export const env = createEnv({
    * useful for Docker builds.
    */
   skipValidation:
-    !!process.env.CI || process.env.npm_lifecycle_event === 'lint',
+    !!process.env.SKIP_ENV_VALIDATION ||
+    !!process.env.CI ||
+    process.env.npm_lifecycle_event === 'lint',
 })
