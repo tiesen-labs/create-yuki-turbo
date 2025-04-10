ff16c3aca97926052bd220ef55fac691cb720768
tiesen243 <ttien56906@gmail.com>
2025-04-10 14:19:42 +0700

    chore(db): use generated folder

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,8 @@
 import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
 import ws from 'ws'
+
+import { PrismaClient } from './generated/client'
 
 neonConfig.webSocketConstructor = ws
 neonConfig.poolQueryViaFetch = true
@@ -25,5 +26,3 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db = globalForPrisma.prisma ?? createPrismaClient()
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
-export type * from '@prisma/client'
