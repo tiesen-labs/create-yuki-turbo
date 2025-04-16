5f1c5a91a540d01e359bdd58886e28c004f98522
tiesen243 <ttien56906@gmail.com>
2025-04-16 21:11:35 +0700

    fix(ci): zod has multiple version

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,5 @@
-import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
-neonConfig.poolQueryViaFetch = true
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL,
