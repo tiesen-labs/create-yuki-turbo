3cbd25de52645450a680ec11ee14b5fd0900f113
tiesen243 <ttien56906@gmail.com>
2025-04-26 16:40:55 +0700

    fix(build): prisma error

--- apps/nextjs/next.config.ts
+++ apps/nextjs/next.config.ts
@@ -5,6 +5,8 @@
 import '@yuki/env'
 
 import type { NextConfig } from 'next'
+// @ts-expect-error - PrismaPlugin is a custom plugin
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -22,6 +24,12 @@ const nextConfig: NextConfig = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+
+  /** Fix Prisma errors when deploying to Vercel */
+  webpack: (config, { isServer }) => {
+    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()]
+    return config
+  },
 }
 
 export default nextConfig
