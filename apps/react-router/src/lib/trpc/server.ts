8213bb2bfaf896473c39664882369c4ba701823f
tiesen243 <ttien56906@gmail.com>
2025-04-26 16:59:46 +0700

    chore: using promise.all for batch prefetch

--- apps/react-router/src/lib/trpc/server.ts
+++ apps/react-router/src/lib/trpc/server.ts
@@ -44,11 +44,13 @@ function batchPrefetch(
 ) {
   const queryClient = getQueryClient()
 
-  for (const queryOptions of queryOptionsArray) {
-    if (queryOptions.queryKey[1]?.type === 'infinite')
-      void queryClient.prefetchInfiniteQuery(queryOptions as never)
-    else void queryClient.prefetchQuery(queryOptions)
-  }
+  void Promise.all(
+    queryOptionsArray.map((queryOptions) => {
+      if (queryOptions.queryKey[1]?.type === 'infinite')
+        void queryClient.prefetchInfiniteQuery(queryOptions as never)
+      else void queryClient.prefetchQuery(queryOptions)
+    }),
+  )
 }
 
 export { api, trpc, prefetch, batchPrefetch }
