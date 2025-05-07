81ae9b9dae4c683dc1aa7630e6098caa27446f7f
tiesen243 <ttien56906@gmail.com>
2025-05-07 14:27:37 +0700

    chore: minor changes

--- apps/react-router/src/routes/_auth.tsx
+++ apps/react-router/src/routes/_auth.tsx
@@ -12,7 +12,7 @@ export default function AuthLayout() {
 
         <CardFooter className="grid gap-2">
           <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-            <span className="text-muted-foreground relative z-10 px-2">
+            <span className="bg-card text-muted-foreground relative z-10 px-2">
               Or continue with
             </span>
           </div>
