72c83e5eed72b4e128c74b5f7718c09dfab0d62a
tiesen243 <ttien56906@gmail.com>
2025-05-08 10:52:43 +0700

    feat(ui): update shadcn color

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -21,11 +21,11 @@ export default function HomePage() {
       <main className="container max-w-2xl py-4">
         <Typography variant="h1" className="text-center">
           Create
-          <span className="text-[#78a9ff]"> Yuki </span>
+          <span className="text-[#46120d] dark:text-[#a96249]"> Yuki </span>
           Turbo
         </Typography>
 
-        <Typography size="lg" className="text-center">
+        <Typography className="text-center text-lg">
           A type-safe fullstack framework for building web applications.
         </Typography>
 
