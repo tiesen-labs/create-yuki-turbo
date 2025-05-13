3f2a2174f851a682a2dfb20a6492155df77df311
tiesen243 <ttien56906@gmail.com>
2025-05-13 15:59:35 +0700

    chore: update form components

--- packages/ui/src/components/form.tsx
+++ packages/ui/src/components/form.tsx
@@ -91,7 +91,7 @@ function useForm<TSchema extends StandardSchemaV1, TData = unknown>(params: {
             {},
           ) as Record<keyof StandardSchemaV1.InferInput<TSchema>, string>,
         })
-        if (onError) await onError('Validation error')
+        if (onError) void onError('Validation error')
         return
       }
 
@@ -303,16 +303,14 @@ const FormItemContext = React.createContext<FormItemContextValue>(
   {} as FormItemContextValue,
 )
 
-function FormItem({ className, ...props }: React.ComponentProps<'fieldset'>) {
+function FormItem({ className, ...props }: React.ComponentProps<'div'>) {
   const id = React.useId()
-  const { isPending } = useFormField()
 
   return (
     <FormItemContext value={{ id }}>
-      <fieldset
+      <div
         data-slot="form-item"
         className={cn('grid gap-1', className)}
-        disabled={isPending}
         {...props}
       />
     </FormItemContext>
@@ -368,6 +366,7 @@ function FormControl(props: React.ComponentProps<'input'>) {
     <Slot
       data-slot="form-control"
       id={formItemId}
+      disabled={isPending}
       aria-describedby={
         !error ? formDescriptionId : `${formDescriptionId} ${formMessageId}`
       }
