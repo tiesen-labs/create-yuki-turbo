134b5fc8133e33dcce5a783d524c34f97adde385
tiesen243 <ttien56906@gmail.com>
2025-05-13 18:43:57 +0700

    fix(ci): typescheck error

--- packages/auth/src/providers/base.ts
+++ packages/auth/src/providers/base.ts
@@ -23,7 +23,6 @@ export abstract class BaseProvider {
 
   protected createCallbackUrl(provider: string) {
     let baseUrl = `http://localhost:${process.env.PORT ?? 3000}`
-    if (typeof window !== 'undefined') baseUrl = window.location.origin
     if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
       baseUrl = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
     if (process.env.VERCEL_URL) baseUrl = `https://${process.env.VERCEL_URL}`
