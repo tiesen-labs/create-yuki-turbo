7428542c529b693ec0e2a34c05c138662d20a2f7
tiesen243 <ttien56906@gmail.com>
2025-05-15 18:02:30 +0700

    chore: minor changes

--- apps/react-router/src/routes/api.trpc.$trpc.ts
+++ apps/react-router/src/routes/api.trpc.$trpc.ts
@@ -2,5 +2,5 @@ import { handlers } from '@yuki/api'
 
 import type { Route } from './+types/api.trpc.$trpc'
 
-export const loader = async ({ request }: Route.LoaderArgs) => handlers(request)
-export const action = async ({ request }: Route.ActionArgs) => handlers(request)
+export const loader = ({ request }: Route.LoaderArgs) => handlers(request)
+export const action = ({ request }: Route.ActionArgs) => handlers(request)
